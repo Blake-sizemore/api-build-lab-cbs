5d7d16f060c3c89b3995893198b0639ce1c3dff6
Blake Sizemore <Blake.sizemore@outlook.com>
2024-01-30 00:49:15 -0500

    basic working lab

--- server/routes/chirps.js
+++ server/routes/chirps.js
@@ -4,6 +4,7 @@ const chirpStore = require('../chirpstore');
 // adds `GET`, `POST`, `PUT`, `DELETE` methods on a router that is created in `chirps.js`.
 
 let router = express.Router();
+// let id = req.params.id; REQ is not definesd this way
 
 router.get("/:id?", (req, res) => {
     let id = req.params.id;
@@ -12,7 +13,7 @@ router.get("/:id?", (req, res) => {
         res.send('chirps if echo, id =' + id);
     } else {
         res.send(chirpStore.GetChirps());
-        // res.send('api/chirps else echo has no id');
+        res.send('api/chirps else echo has no id');
     }
 });
 // Working so far
@@ -20,5 +21,29 @@ router.post('/', (req, res) => {
     chirpStore.CreateChirp(req.body);
     res.sendStatus(200);
 });
+
+router.put('/:id?', (req, res) => {
+    let id = req.params.id;
+    res.json(chirpStore.UpdateChirp(id,req.body));
+    res.sendStatus(200);
+});
+
+router.put('/:id?', (req, res) => {
+    let id = req.params.id;
+    res.json(chirpStore.UpdateChirp(id,req.body));
+    res.sendStatus(200);
+});
+
+router.delete('/:id?',(req,res)=>{
+    let id = req.params.id;
+    res.json(chirpStore.DeleteChirp(id));
+    res.sendStatus(200);
+})
+
+// router.delete('/:id?', (req, res) => {
+//     chirpStore.DeleteChirp(id);
+//     res.sendStatus(200);
+// });
+
 
 module.exports = router;
